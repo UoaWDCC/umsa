449a1bba87da6170fe289bf02c8b792dd8367af3
alannasantoso <alannasantoso22@gmail.com>
2026-03-23 00:39:16 +1300

    Remove .env from tracking

---
+++ server/db/connection.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+} catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+}
+
+let db = client.db("umsa"); 
+
+export default db
