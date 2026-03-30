c07fae4a451af793f3687f8ac36ede84dcdd5c32
nicholas-garcia-scholtz <ngar756@aucklanduni.ac.nz>
2026-03-31 01:35:14 +1300

    take this as my resignation

--- server/dist/routes/members.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import db from "../db/connection.js";
-const router = express.Router();
-router.get("/", async (req, res) => {
-    let collection = db.collection("members");
-    let results = await collection.find({}).toArray();
-    res.status(200).send(results);
-});
-export default router;
