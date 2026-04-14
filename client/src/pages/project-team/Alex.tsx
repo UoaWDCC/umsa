3bbd3348f723ace8c27bfb140175f481b6824a00
Alex <alexl210107@gmail.com>
2026-04-14 19:08:10 +1200

    feat:Fixed alex about me

--- client/src/pages/project-team/Alex.tsx
+++ client/src/pages/project-team/Alex.tsx
@@ -1,4 +1,4 @@
-import { FaInstagram, FaGithub, FaLinkedin, FaFilm } from "react-icons/fa";
+import { FaInstagram, FaGithub, FaLinkedin, FaFilm, FaSpotify } from "react-icons/fa";
 import MemberStatsTable from "../../components/StatsTable";
 import bgImage from "../../assets/about-us/IMG_7577.jpeg"; 
 import AlexImage from "../../assets/about-us/P1010200.jpeg";
@@ -6,16 +6,15 @@ import { useEffect } from "react";
 
 export default function Alex(){
     useEffect(() => {
-    document.title = "Alexander(#1 Developer) | UMSA";
+    document.title = "Alex(#1 Developer) | UMSA";
     }, []);
 
     return(
         <>
-        <div className="relative h-screen w-full flex items-center justify-center rounded-lg">
+        <div className="relative min-h-screen w-full flex flex-col items-center justify-center rounded-lg">
             {/* bg image */}
-            <div style={{ backgroundImage: `url(${bgImage})` }} className="absolute p-20 h-screen w-full opacity-50 flex item-center justify-center rounded-lg bg-cover bg-center">
-            
-            </div>
+            <div style={{ backgroundImage: `url(${bgImage})` }} className="absolute inset-0 bg-cover bg-center opacity-50 rounded-lg"></div>
+
             {/* contents */}
             <div className ="relative h-full max-w-[700px] flex flex-col">
                 {/* header */}
@@ -24,12 +23,12 @@ export default function Alex(){
                 </div>
 
                 {/* main */}
-                <div className="md:grid grid-cols-3 flex flex-col gap-10 item-center justify-center">
+                <div className="md:grid grid-cols-3 flex flex-col w-full gap-10 items-center justify-center">
                     {/* beautiful photo of me */}
-                    <img src={AlexImage} className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl shadow-lg shadow-xl/30"/>
+                    <img src={AlexImage} className="col-span-1 object-cover w-full max-w-sm md:max-w-none h-full min-h-[300px] rounded-2xl shadow-lg shadow-xl/30"/>
 
                     {/* stats */}
-                    <div className="col-span-2 max-w-md bg-gray-900 p-6 rounded-lg shadow-lg shadow-xl/30">
+                    <div className="col-span-2 w-full max-w-sm md:max-w-none flex flex-col bg-gray-900 p-6 rounded-lg shadow-lg shadow-xl/30">
                     
                         <MemberStatsTable 
                         stats={{
@@ -37,10 +36,10 @@ export default function Alex(){
                             age: 19,
                             ethnicity: "Korean🇰🇷🇰🇷🇰🇷",
                             year: "First year",
-                            degree: "compsci & IT mangaement",
+                            degree: "compsci & IT management(im gonna drop this for Finance conjoint)",
                             typingSpeed: "106 wpm",
-                            favouritePaper: "n/a",
-                            favouriteFood: "Marlboro Gold + Guinness",
+                            favouritePaper: "A4",
+                            favouriteFood: "3am maccas mish(Big Mac, 2 Cheeseburgers, medium fries, 6pcs chicken nugget with 2 sweet and sour sauce and medium Sprite zero)",
                     
                         }}
                         />       
@@ -48,29 +47,53 @@ export default function Alex(){
                 </div>
 
                 {/* about me */}
-                <div> 
-                    <p className="text-sm mt-4">
-                        insert really cool and interesting about me here.
+                <div className="w-full mb-32"> 
+                    <p className="text-sm mt-4 leading-relaxed">
+                        pretend like i have a really cool and interesting about me here.<br/>
+                        Snorlax RAWRRRR<br/>
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣿⣶⣦⣄⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⣶⣾⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⠀⠀⢀⡀⣄⠀⠀⠀⠀⠀⠀⠀⣿⣿⠟⠉⠀⢀⣀⠀⠀⠈⠉⠀⠀⣀⣀⠀⠀⠙⢿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⣀⣶⣿⣿⣿⣾⣇⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⣼⡏⠀⠀⠀⣀⣀⣉⠉⠩⠭⠭⠭⠥⠤⢀⣀⣀⠀⠀⠀⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⣿⠷⠒⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠒⠼⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⠈⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⠀⢹⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⠀⠀⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀<br/>
+                        ⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣶⣤⣄⣠⣤⣤⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀<br/>
+                        ⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀<br/>
+                        ⠀⠀⣀⠀⢸⡿⠿⣿⡿⠋⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠻⠿⠟⠉⢙⣿⣿⣿⣿⣿⣿⡇<br/>
+                        ⠀⠀⢿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⢿⡿⣿⠳⠀<br/>
+                        ⠀⠀⡞⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣇⡀⠀⠀<br/>
+                        ⢀⣸⣀⡀⠀⠀⠀⠀⣠⣴⣾⣿⣷⣆⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣰⣿⣿⣿⣿⣷⣦⠀⠀⠀⠀⢿⣿⠿⠃⠀<br/>
+                        ⠘⢿⡿⠃⠀⠀⠀⣸⣿⣿⣿⣿⣿⡿⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⢻⣿⣿⣿⣿⣿⣿⠂⠀⠀⠀⡸⠁⠀⠀⠀<br/>
+                        ⠀⠀⠳⣄⠀⠀⠀⠹⣿⣿⣿⡿⠛⣠⠾⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠳⣄⠙⠛⠿⠿⠛⠉⠀⠀⣀⠜⠁⠀⠀⠀⠀<br/>
+                        ⠀⠀⠀⠈⠑⠢⠤⠤⠬⠭⠥⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠢⠤⠤⠤⠒⠊⠁⠀⠀⠀⠀⠀⠀
                     </p>
                 </div>
 
                 {/* social media section */}
-                <div className="absolute bottom-10 inset-x-0 flex flex-col items-center justify-center">
+                <div className="w-full flex flex-col items-center pt-20 mb-2">
                     <h1 className="text-3xl mb-1 font-bold italic text-left">✨Socials✨</h1>
                     <div className="flex grid-cols-4 gap-4 mt-1">
                                 
                         {/* the <a></a> tags are for creating links */}
-                        <a href="https://www.instagram.com/aiex.lee/" target="_blank" rel="noopener noreferrer">
-                               <FaInstagram className="text-3xl text-grey-300 hover:text-white" />
-                        </a>
                         <a href="https://github.com/AiexProjects" target="_blank" rel="noopener noreferrer">
                             <FaGithub className="text-3xl text-grey-300 hover:text-white" />
                         </a>
                         <a href="https://www.linkedin.com/in/alex-lee-571736358/" target="_blank" rel="noopener noreferrer">
                             <FaLinkedin className="text-3xl text-grey-300 hover:text-white" />
                         </a>
+                        <a href="https://www.instagram.com/aiex.lee/" target="_blank" rel="noopener noreferrer">
+                               <FaInstagram className="text-3xl text-grey-300 hover:text-white" />
+                        </a>
                         <a href="https://letterboxd.com/alexree121/" target="_blank" rel="noopener noreferrer">
                             <FaFilm className="text-3xl text-grey-300 hover:text-white" />
+                        </a>
+                        <a href="https://open.spotify.com/user/cq4c5m8ihueunpkfykn1l3s2f" target="_blank" rel="noopener noreferrer">
+                            <FaSpotify className="text-3xl text-grey-300 hover:text-white" />
                         </a>
                     </div>
                 </div>
