6e081a331a3b16392d3216a3d17c97d23927cf60
Terrence Wu <terrencewu1738@gmail.com>
2026-04-14 15:50:54 +1200

    style: finished layout

--- client/src/pages/project-team/Terrence.tsx
+++ client/src/pages/project-team/Terrence.tsx
@@ -22,27 +22,27 @@ export default function Terrence() {
 
         {/* header section */}
         <div className="flex flex-col items-center mb-8">
-            <h1 className="text-3xl font-bold italic mb-5">hi, my name is terrence :D</h1>
+            <h1 className="text-3xl font-bold italic mb-5">Terrence :o</h1>
         </div>
 
         {/* stats section */}
-        <div className="md:grid grid-cols-3 gap-10 item-center justify-center">
+        <div className="md:grid gap-10 item-center justify-center">
             {/* we use an img component to display my photo */}
-            <img src={TerrencePhoto} className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl"
+            <img src={TerrencePhoto} className="col-span-2 object-cover md:mb-0 mb-5 w-full rounded-2xl"
             />
             {/* heres where components come in - we use a pre built stats component,
                 and just fill in the props (stats). check /components/StatsTable.tsx to see how it works */}
-            <div className="col-spam-2 max-w-md bg-gray-900 p-6 rounded-lg">
+            <div className="col-span-2 w-156 bg-gray-900 p-6 rounded-lg item-center justify-center">
                 <MemberStatsTable
                 stats={{
-                    fullName: "terrence wu",
+                    fullName: "Terrence u",
                     age: 17,
-                    ethnicity: "chinese",
-                    year: "first year",
-                    degree: "computer science",
+                    ethnicity: "Chinese",
+                    year: "First year",
+                    degree: "Computer Science",
                     typingSpeed: "135wpm",
-                    favouritePaper: "compsci120",
-                    favouriteFood: "fried chicken and burgers",
+                    favouritePaper: "CompSci120",
+                    favouriteFood: "Fried chicken and Burgers",
                 }}
                 />
 
@@ -51,29 +51,33 @@ export default function Terrence() {
 
         {/* abut paragraph section */}
         
-        <div>
-            <p className="text-sm mt-10">
-                Temporary about me section
+        <div className="md: grid gap-10 item-center justify-center">
+            <p className="md:text-sm mt-10 bg-gray-900 p-4 rounded-lg">
+                Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section 
+                Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section 
+                Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section 
+                Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section 
+                Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section Temporary about me section 
             </p>
         </div>
 
         {/* social media section */}
         <div>
             <h1 className="text-3xl mt-10 mb-5 font-bold text-left">Social Links:</h1>
-            <h1 className="text-l mt-10 mb-5 font-bold italic text-left">add me on Linkedin pls</h1>
+            {/* <h1 className="text-l mt-10 mb-5 font-bold italic text-left">add me on Linkedin pls</h1> */}
             <div className="flex grid-cols-4 gap-4 mt-4">
 
                 {/* the <a></a> tags are for creating links */}
                 <a href="https://www.instagram.com/_tw1738" target="_blank" rel="noopener noreferrer">
-                    <FaInstagram className="text-3xl text-gray-300 hover:text-white" />
+                    <FaInstagram className="text-4xl text-gray-300 transform duration-350 ease-in-out hover:text-white" />
                 </a>
 
                 <a href="https://github.com/Twu1738" target="_blank" rel="noopener noreferrer">
-                    <FaGithub className="text-3xl text-gray-300 hover:text-white" />
+                    <FaGithub className="text-4xl text-gray-300 transform duration-350 ease-in-out hover:text-white" />
                 </a>
 
-                <a href="https://www.linkedin.com/in/terrence-wu" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin className="text-3xl text-gray-300 hover:text-white" />
+                <a href="https://www.linkedin.com/in/terrence-wu-5657a0376/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="text-4xl text-gray-300 transform duration-350 ease-in-out hover:text-white" />
                 </a>
 
             </div>
