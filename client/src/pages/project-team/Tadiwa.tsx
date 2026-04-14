1d694de4ae61dfeb0aa2e95402b921ae964c8b79
Tadiwa <tmud241@aucklanduni.ac.nz>
2026-04-14 21:54:04 +1200

    restyling

--- client/src/pages/project-team/Tadiwa.tsx
+++ client/src/pages/project-team/Tadiwa.tsx
@@ -11,12 +11,13 @@ import pinkme from "../../assets/icons/pinkhair.png";
 export default function Tadiwa(){
     return(
         <>
-<div className="bg-pink-200 style.fontSize = 5px">  
+<div className="bg-pink-200 italic text-black">  
     <h1> TADIWA&apos;S ABOUT ME PAGE </h1>
-    <p> Hello </p>
+
+    <p> HI </p>
     <p> My name is tadiwa. I like cats,music,gym and FOOODDDDDD.My favourite food is shibbys and sushi and mini eggs and fries and greek salad
     I work at countdown (woolies) and I used to work at burgerfuel as a shift manager </p>
-    <div>
+<div className="text-black">
         <MemberStatsTable 
                         stats={{
                             fullName: "Tadiwa Mudapakati",
@@ -33,18 +34,19 @@ export default function Tadiwa(){
                 
 
     </div>
-           
 
+ <div className="flex grid-cols-4 gap-4 mt-4" > 
       <img 
-            src={me} 
-            className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl"
+src={me} 
+            className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl w-32 h-32 object-cover"
             />  
+            <p> This is me </p>
         <img 
             src={pinkme}
-            className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl"
+            className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl w-32 h - 32 object-cover"
             />           
-     
-                
+     <p> This is me with pink hair </p>
+</div>  
                    
                     please add my linkedin #verylongroadto500
             
@@ -54,7 +56,7 @@ export default function Tadiwa(){
                             <div className="flex grid-cols-4 gap-4 mt-4">
                                 
                                 {/* the <a></a> tags are for creating links */}
-                                <a href="https://www.instagram.com/alanna_ds" target="_blank" rel="noopener noreferrer">
+                                <a href="https://www.instagram.com/tadiwaxm" target="_blank" rel="noopener noreferrer">
                                     <img 
             src={instapic} 
             className="col-span-1 object-cover md:mb-0 mb-5 h-full rounded-2xl"
