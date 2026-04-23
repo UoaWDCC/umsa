6d5fb698eccb889de86d73fe754e8252ad647fa1
Terrence Wu <terrencewu1738@gmail.com>
2026-04-23 14:41:03 +1200

    feat: embedded the google form

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,11 @@
 export default function SignUp(){
     return(
         <>
-        <p>Sign Up</p>
+        <div className="bg-gray-90 h-screen">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfbAK-wCfqe7MHDuEQs2RHfgQKnNH2eROErsA2FQwixi-isPg/viewform?embedded=true" 
+        className="border-none" width="100%" height="100%">
+        </iframe>
+        </div>
         </>
     )
 }
