3115dcc08464f129456b7ce81b492bb8b2190598
nryuzl <prunanero@gmail.com>
2026-04-23 14:05:01 +1200

    Create route for Sponsors page

--- client/src/pages/Sponsors.tsx
+++ client/src/pages/Sponsors.tsx
@@ -4,4 +4,16 @@ export default function Sponsors() {
     useEffect(() => {
         document.title = "Sponsors | UMSA";
         }, []);
+    
+        return (
+            <div className="flex flex-col items-center justify-center min-h-screen text-white">
+            <h1 className="text-4xl font-bold">Our Sponsors</h1>
+            <p className="mt-4 text-gray-400">
+                UMSA Sponsors Page
+            </p>
+            <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-8">
+                {/* SPONSORS WILL GO HERE */}
+            </div>
+        </div>
+        );
 }
