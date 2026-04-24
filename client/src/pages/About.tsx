87fff4269dc1c6be87bf6e7cdeabc7067e0b8127
Tadiwa <tmud241@aucklanduni.ac.nz>
2026-04-24 18:52:28 +1200

    feat: flexboxs + header

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react"; 
+import'./about.css';
+
 export default function About(){
+    useEffect(() => { document.title = "About | UMSA";},
+    []);
 
     return(
-        
-<p> test yay  </p>
+<div>
+    <div className=" header">
+        <header style={{ fontSize: "2rem" , marginBottom: "3rem"}} >
+            UMSA
+        </header>
+    </div>
+    <div className = " biggest flex container " style={{ color: "black"}}> 
+        <div className = "box" id="history" >
+            <p> history</p> 
+            <p> The history of umsa is as follows:</p>
+        </div>
+
+        <div className = "box" id="vision">  Vision </div>
+        <div className = "box" id= "purpose"> Purpose </div>
+    </div>
+</div>
+
     )
 }
 
