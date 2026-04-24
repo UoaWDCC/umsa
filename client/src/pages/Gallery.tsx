ae800a1a97f898fd96cec9204768e58a84573c6a
Terrence Wu <terrencewu1738@gmail.com>
2026-04-24 12:09:02 +1200

    feat: added some test images to gallery

--- client/src/pages/Gallery.tsx
+++ client/src/pages/Gallery.tsx
@@ -1,7 +1,21 @@
+import Test from '../assets/GalleryTest.png'
+import Test2 from '../assets/GalleryTest2.png'
+
 export default function Gallery(){
    return (
     <>
-    <div></div>
+    <div className="flex px-5 gap-10 flex-wrap justify-center" >
+    <img src={Test} alt="test-photo"/>
+    <img src={Test2} alt="test-photo"/>
+    <img src={Test} alt="test-photo"/>
+    <img src={Test2} alt="test-photo"/>
+    <img src={Test} alt="test-photo"/>
+    <img src={Test2} alt="test-photo"/>
+    <img src={Test} alt="test-photo"/>
+    <img src={Test2} alt="test-photo"/>
+    <img src={Test} alt="test-photo"/>
+    <img src={Test2} alt="test-photo"/>
+    </div>
     </>
    )
 }
