e5b3eec8fa0c6411b342f80b86cc5886e76767b4
Terrence Wu <terrencewu1738@gmail.com>
2026-04-24 12:34:18 +1200

    feat: remade how images are displayed
    had to use a little bit of google but this should be a bit better? hopefully

--- client/src/pages/Gallery.tsx
+++ client/src/pages/Gallery.tsx
@@ -1,21 +1,27 @@
-import Test from '../assets/GalleryTest.png'
+import Test1 from '../assets/GalleryTest.png'
 import Test2 from '../assets/GalleryTest2.png'
 
 export default function Gallery(){
-   return (
-    <>
-    <div className="flex px-5 gap-10 flex-wrap justify-center" >
-    <img src={Test} alt="test-photo"/>
-    <img src={Test2} alt="test-photo"/>
-    <img src={Test} alt="test-photo"/>
-    <img src={Test2} alt="test-photo"/>
-    <img src={Test} alt="test-photo"/>
-    <img src={Test2} alt="test-photo"/>
-    <img src={Test} alt="test-photo"/>
-    <img src={Test2} alt="test-photo"/>
-    <img src={Test} alt="test-photo"/>
-    <img src={Test2} alt="test-photo"/>
-    </div>
-    </>
+    const images = [
+        {src: Test1, alt: "test-photo"}, 
+        {src: Test2, alt: "test-photo"}, 
+        {src: Test1, alt: "test-photo"}, 
+        {src: Test2, alt: "test-photo"}, 
+        {src: Test1, alt: "test-photo"}, 
+        {src: Test2, alt: "test-photo"}, 
+        {src: Test1, alt: "test-photo"}, 
+        {src: Test2, alt: "test-photo"},
+        {src: Test1, alt: "test-photo"},
+        {src: Test2, alt: "test-photo"}
+    ]
+    return (
+        <>
+        <div className="flex px-5 gap-10 flex-wrap justify-center" >
+            {images.map((image, index) => (
+                <img key={index} src={image.src} alt={image.alt} 
+                className="w-48 h-48 object-cover" />
+            ))}
+        </div>
+        </>
    )
 }
