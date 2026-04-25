4ea87a98e25f312c99b328b642f1b60d499f697e
Twu1738 <terrencewu1738@gmail.com>
2026-04-25 20:38:10 +1200

    feat: signup page creation & routing
    * feat: set up the router for signups
    
    * feat: added a button to signups
    
    * feat: embedded the google form
    
    * style: router spacing fixed
    
    ---------
    
    Co-authored-by: alanna <139197719+alannasantoso@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './layouts/RootLayout'
 import App from './App'
 import ProjectTeam from './pages/Project-Team'
+import './index.css'
+import SignUp from './pages/SignUp'
+import Gallery from './pages/Gallery'
+
+{/* project team imports */}
+import Pruna from "./pages/project-team/Pruna";
 import Alanna from './pages/project-team/Alanna'
 import Tadiwa from './pages/project-team/Tadiwa'
 import Terrence from './pages/project-team/Terrence'
-import './index.css'
-import Gallery from './pages/Gallery'
-import Pruna from "./pages/project-team/Pruna";
 
 {/* here's where we set up all our routing */}
 const router = createBrowserRouter([
@@ -17,13 +20,15 @@ const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true,             element: <App /> },
-      { path: 'project-team',    element: <ProjectTeam /> },
-      { path: 'alanna',          element: <Alanna /> },
-      { path: 'gallery',         element: <Gallery /> },
-      { path: 'tadiwa', element: <Tadiwa />},
-      { path: 'terrence',         element: <Terrence />},
-      { path: "pruna", element: <Pruna /> },
+      {index: true,             element: <App /> },
+      {path: 'project-team',    element: <ProjectTeam /> },
+      {path: 'sign-up',         element: <SignUp />},
+      {path: 'gallery',         element: <Gallery />},
+      
+      {path: 'alanna',          element: <Alanna /> },
+      {path: 'tadiwa', element: <Tadiwa />},
+      {path: 'terrence',         element: <Terrence />},
+      {path: "pruna", element: <Pruna />},
     ],
   },
 ]);
