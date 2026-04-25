9c171a2e556745680fc5243cd8f8177feae73fec
Alex <alexl210107@gmail.com>
2026-04-25 21:40:04 +1200

    feat: add events stored in array, filter for upcoming or past events, comments

--- client/src/components/EventsElement.tsx
+++ client/src/components/EventsElement.tsx
@@ -1,3 +1,4 @@
+{/* types for events variables */}
 type EventsElement = {
     eventName: string;
     eventImage: string;
@@ -6,28 +7,31 @@ type EventsElement = {
     eventIsDone: boolean;
 };
 
+{/* properties */}
 type Props = {
     element: EventsElement;
 };
 
+{/* eventsInfo function that define how each event element should look in the grid */}
 export default function EventsInfo({ element }: Props) {
     return(
+        <>
+        {/* opens link when clicked, and change visual depending on the true or false argument of element.eventIsDone */}
         <a href={element.eventLink} target="_blank" rel="noopener noreferrer" 
         className={`flex flex-col h-full w-full cursor-pointer rounded-lg p-5 
         ${element.eventIsDone ? 'bg-gray-700 opacity-50' : 'bg-gray-900 hover:bg-gray-800 transition-all duration-300 hover:scale-105'}`}
         >
-            <div className="">
-                <h1 className="rounded-md w-full object-cover">
-                    {element.eventName}
-                </h1>
+            <h1 className="font-bold mb-2">
+                {element.eventName}
+            </h1>
 
-                <img className=""
-                    src={element.eventImage} alt={element.eventName}
-                />
-                <p className="text-sm mt-2">
-                    {element.eventDescription}
-                </p>
-            </div>
+            <img className="rounded-md object-cover"
+                src={element.eventImage} alt={element.eventName}
+            />
+            <p className="text-sm mt-2 hyphens-auto">
+                {element.eventDescription}
+            </p>
         </a>
+        </>
     )
 }
--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -2,6 +2,43 @@ import { useEffect } from "react";
 import EventsInfo from "../components/EventsElement"; 
 import EventImage from "../assets/about-us/Copy of UMSA_Guitar.png"
 
+{/* constant events array */}
+const events = [
+    {
+        eventName: "Jalinan Raya",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DWPgAxmky6q/?img_index=1",
+        eventDescription: "very cool and very descriptive description",
+        eventIsDone: false,
+    },
+    {
+        eventName: "Bersatu Trials",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DV7o4eZmPHH/?img_index=1",
+        eventDescription: "even cooler and even more descriptive description",
+        eventIsDone: false,
+    },
+    {
+        eventName: "Clash of UMSA",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: false,
+    },
+    {
+        eventName: "Clash of UMSA",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 supercalifragilisticexpialidocious Hippopotomonstrosesquippedaliophobia Pneumonoultramicroscopicsilicovolcanoconiosis",
+        eventIsDone: true,
+    },
+];
+
+{/* use .map() to make new array depending on events.eventIsDone is true or false */}
+const upcomingEvents = events.filter((events) => events.eventIsDone == false);
+const pastEvents = events.filter((events) => events.eventIsDone == true);
+
+
 export default function Events() {
 
     useEffect(() => {
@@ -9,44 +46,31 @@ export default function Events() {
         }, []);
 
     return(
+        <>
         <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col w-full">
-                <h1 className="text-3xl font-bold mb-4">
-                    Events
+            <div className="flex flex-col w-4/5">
+                <h1 className="text-3xl font-bold mb-6">
+                    Upcoming Events
                 </h1>
             </div>
+            {/* grid only containing upcoming events, using the upcomingEvents grid */}
             <div className="w-4/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                <EventsInfo
-                    element={{
-                        eventName: "Jalinan Raya",
-                        eventImage: EventImage,
-                        eventLink: "https://www.instagram.com/p/DWPgAxmky6q/?img_index=1",
-                        eventDescription: "very cool and very descriptive description",
-                        eventIsDone: false,
-
-                    }}
-                />
-                <EventsInfo
-                    element={{
-                        eventName: "Bersatu Trials",
-                        eventImage: EventImage,
-                        eventLink: "https://www.instagram.com/p/DV7o4eZmPHH/?img_index=1",
-                        eventDescription: "even cooler and even more descriptive description",
-                        eventIsDone: false,
-
-                    }}
-                />
-                <EventsInfo
-                    element={{
-                        eventName: "Clash of UMSA",
-                        eventImage: EventImage,
-                        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
-                        eventDescription: "even even cooler and way wayyyyy more descriptive description",
-                        eventIsDone: true,
-
-                    }}
-                />
+                {upcomingEvents.map((upcomingEvent) => (
+                    <EventsInfo key={upcomingEvent.eventName} element={upcomingEvent}/>
+                ))}
+            </div>
+            <div className="flex flex-col w-4/5">
+                <h1 className="text-3xl font-bold mt-12 mb-6">
+                    Past Events
+                </h1>
+            </div>
+            {/* grid only containing past events, using the pastEvents grid */}
+            <div className="w-4/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {pastEvents.map((pastEvent) => (
+                    <EventsInfo key={pastEvent.eventName} element={pastEvent}/>
+                ))}
             </div>
         </div>
+        </>
     )
 }
