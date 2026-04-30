4997d40883f62dfb8b4a5cb35ecfdc636b86fbc1
jzchenn <jackyc.pata@gmail.com>
2026-04-30 13:20:56 +1200

    feat: add role and section

--- client/src/components/MemberInfo.tsx
+++ client/src/components/MemberInfo.tsx
@@ -3,6 +3,7 @@ import { FaInstagram, FaLinkedin} from "react-icons/fa";
 type MemberInfo = {
   img: string;
   fullName: string;
+  role: string;
   linkedin: string; 
   instagram: string;
 };
@@ -18,6 +19,7 @@ return (
     <div className="flex flex-col items-center gap-4">
         <img className="w-64 h-64 rounded-4xl " src={stats.img}/>
         <p>{stats.fullName}</p>
+        <p className="text-lg font-bold">{stats.role}</p>
         <div className="flex">
         <a href={stats.instagram} target="_blank" rel="noopener noreferrer">
             <FaInstagram className="text-3xl text-black-300 hover:text-white" />
