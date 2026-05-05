cac6b271988c21062c1a226c724051a4411db966
Tadiwa <tmud241@aucklanduni.ac.nz>
2026-05-05 17:33:52 +1200

    feat: css to tailwind update

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,37 +1,39 @@
 import { useEffect } from "react"; 
-import'./about.css';
+
 
 export default function About(){
-    useEffect(() => { document.title = "About | UMSA";},
+    useEffect(() => { document.title = "About | gotUMSA";},
     []);
 
     return(
-<div className="about">
-    <div className=" header">
+<div className="mx-auto " >
+    <div className=" header mb-8">
         <h1> UMSA </h1>
         <p> Welcome to umsa about us page </p>
     </div>
 
 
-    <div className = "main" style={{ color: "white"}}> 
-
-        <div className = "box" id="history" >
+    <div className = "flex flex-col gap-8" style={{ color: "white"}}> 
+        
+        <div className = "p-6 border border-white box-border w-full min-h-[200px]" >
             <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}> History </h2> 
             <p> bla bla bla bla </p>
         </div>
 
-        <div className = "box" id="vision"> 
-            <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}>Vision</h2>
-            <p> bla bla bla bla bla bla bla </p>
+        <div className = "p-6 border border-white box-border w-full min-h-[200px]" >
+ 
+           <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}>Vision</h2>
+            <p> bla b dev la bla bla bla bla bla </p>
              </div>
 
-        <div className = "box" id= "purpose"> 
+        <div className = "p-6 border border-white box-border w-full min-h-[200px]"> 
             <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}>Purpose</h2>
             <p> bla bla bla bla  </p>
              </div>
-        
+         
+
     </div>
-    <div className="link">
+    <div className="text-lg mt-8">
         <h2> Meet the team:  </h2>
         <a href = " https://github.com/UoaWDCC/umsa/tree/tadiwa/about-us-page/"> Press here </a>
          </div>
