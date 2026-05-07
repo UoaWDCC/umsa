34932ce4d49a88adb73f94c267270c3dd8659f5b
Eric Zheng <eric.zheng@outlook.com>
2026-05-07 14:56:24 +1200

    feat: cleaned up back-end and added security (helmet, CORS, etc.)

--- server/src/routes/developers.ts
+++ server/src/routes/developers.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import Developer, { IDeveloperDocument } from '../models/Developer.js';
+import { sendError } from '../utils/apiError.js';
 
 const router = Router();
 
@@ -8,17 +10,24 @@ router.get('/', async (_req: Request, res: Response) => {
     const developers = await Developer.find();
     res.json(developers);
   } catch {
-    res.status(500).json({ message: 'oh hell nah' });
+    sendError(res, 500, 'INTERNAL_ERROR', 'Unable to fetch developers');
   }
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
   try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return sendError(res, 400, 'BAD_REQUEST', 'Invalid id');
+    }
+
     const developer = await Developer.findById(req.params.id);
-    if (!developer) return res.status(404).json({ message: 'oh hell nah' });
+    if (!developer) {
+      return sendError(res, 404, 'NOT_FOUND', 'Developer not found');
+    }
+
     res.json(developer);
   } catch {
-    res.status(500).json({ message: 'oh hell nah' });
+    sendError(res, 500, 'INTERNAL_ERROR', 'Unable to fetch developer');
   }
 });
 
@@ -27,8 +36,8 @@ router.post('/', async (req: Request<object, object, IDeveloperDocument>, res: R
     const developer = new Developer(req.body);
     await developer.save();
     res.status(201).json(developer);
-  } catch{
-    res.status(400).json({ message: 'oh hell nah' });
+  } catch {
+    sendError(res, 400, 'VALIDATION_ERROR', 'Invalid developer payload');
   }
 });
 
