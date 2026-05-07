c87996f98b01de7ad11455ef1da8beb4726eae18
Tadiwa <tmud241@aucklanduni.ac.nz>
2026-05-07 16:09:41 +1200

    feat: adding photos

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react"; 
-
+import sunsetphoto from "../assets/about-us/sunset.jpg";
 
 export default function About(){
     useEffect(() => { document.title = "About | gotUMSA";},
     []);
 
     return(
+       
 <div className="mx-auto " >
     <div className=" header mb-8">
         <h1> UMSA </h1>
@@ -14,18 +15,27 @@ export default function About(){
 
 
     <div className = "flex flex-col gap-8" style={{ color: "white"}}> 
-        
-        <div className = "p-6 border border-white box-border w-full min-h-[200px]" >
+
+   
+
+
+      <div className = " flex items-start"> 
+        <div className = "p-6 border border-white box-border w-64 ml auto min-h-[200px]" >
             <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}> History </h2> 
             <p> bla bla bla bla </p>
         </div>
+        <div className = " ml-10">
+            <img src= {sunsetphoto}/>
+        </div>
+      </div>
 
+      <div className = " flex items-start "> 
         <div className = "p-6 border border-white box-border w-full min-h-[200px]" >
  
            <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}>Vision</h2>
             <p> bla b dev la bla bla bla bla bla </p>
              </div>
-
+ </div>
         <div className = "p-6 border border-white box-border w-full min-h-[200px]"> 
             <h2 style= {{ fontSize: "1.5rem" , position: "relative" , top: "-10px" }}>Purpose</h2>
             <p> bla bla bla bla  </p>
