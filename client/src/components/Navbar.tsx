86bbfb6e5c5a1fce7a65c053d566e6ed4ba2568b
Terrence Wu <terrencewu1738@gmail.com>
2026-05-07 15:11:32 +1200

    feat: added adropdow depending on screensize

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,25 +1,34 @@
 import { Link, NavLink } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
 
 export default function Navbar() {
   const linkClass = ({ isActive }: { isActive: boolean }) =>
     `transition-colors hover:text-white ${isActive ? 'text-white font-semibold' : 'text-gray-400'}`;
-
+  
   return (
-    
-    <nav className="mx-6 py-4">
-      <div className="hidden md:flex space-x-4">
-      <div className="max-w-6xl mx-auto flex items-center justify-between">
-        <Link to="/" className="text-xl font-bold tracking-tight">
-          UMSA
-        </Link>
-        <div className="flex gap-6 text-sm">
-          <NavLink to="/" end className={linkClass}>home</NavLink>
-          <NavLink to="/project-team" className={linkClass}>project team</NavLink>
-          <NavLink to="/sign-up" className={linkClass}>Sign Up</NavLink>
-          <NavLink to="/gallery" className={linkClass}>Gallery</NavLink>
-        </div>
+  
+  <nav className="mx-6 py-4">
+    <div className="max-w-6xl mx-auto flex items-center justify-between">
+      <Link to="/" className="text-xl font-bold tracking-tight">
+        UMSA
+      </Link>
+      {/* shows on desktop screens (hidden at first) */}
+      <div className="hidden md:flex gap-6 text-sm">
+        <NavLink to="/" end className={linkClass}>home</NavLink>
+        <NavLink to="/project-team" className={linkClass}>project team</NavLink>
+        <NavLink to="/sign-up" className={linkClass}>Sign Up</NavLink>
       </div>
-      </div>
-    </nav>
+
+      {/* shows on small screens only (is hidden once screen size is big) */}
+      <select className="block md:hidden">
+        <option value="">Dropdown</option>
+        <option value="/">home</option>
+        <option value="/project-team">project team</option>
+        <option value="/sign-up">Sign Up</option>
+      </select>
+
+    </div>
+  </nav>
   );
 }
