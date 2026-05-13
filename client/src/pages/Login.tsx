5385207513e8b5ec0d94e69ae7e4be41975a2d8f
jzchenn <jackyc.pata@gmail.com>
2026-05-13 16:54:01 +1200

    feat: added input fields

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,19 @@ export default function Login(){
     return(
         <>
         <h1 className="text-3xl font-bold mb-8">Login</h1>
+        <div className="flex flex-col gap-4 w-1/2 mx-auto">
+        <p>Username</p>
+        <input className="border border-gray-300 rounded py-2 px-4"
+            placeholder="Enter username"
+        />
+        <p>Password</p>
+        <input className="border border-gray-300 rounded py-2 px-4"
+            placeholder="Enter password"
+        />
+        <button className="bg-blue-500 text-white rounded py-2 px-4 mt-4 cursor-pointer">
+            Login
+        </button>
+        </div>
         </>
     )
 }
