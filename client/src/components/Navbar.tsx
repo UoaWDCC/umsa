84fa1dd1875f17d76cd85c2f5b0bdb9f8a9e94ee
Terrence Wu <terrencewu1738@gmail.com>
2026-05-13 15:49:22 +1200

    feat: revamped dropdown
    each dropdown option now goes to a different page via route

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,18 +1,26 @@
 import { Link, NavLink } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-
+import React, { useState } from 'react';
 
 export default function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    }
+
   const linkClass = ({ isActive }: { isActive: boolean }) =>
     `transition-colors hover:text-white ${isActive ? 'text-white font-semibold' : 'text-gray-400'}`;
-  
+
   return (
   
   <nav className="mx-6 py-4">
     <div className="max-w-6xl mx-auto flex items-center justify-between">
+      
       <Link to="/" className="text-xl font-bold tracking-tight">
         UMSA
       </Link>
+
       {/* shows on desktop screens (hidden at first) */}
       <div className="hidden md:flex gap-6 text-sm">
         <NavLink to="/" end className={linkClass}>home</NavLink>
@@ -21,12 +29,47 @@ export default function Navbar() {
       </div>
 
       {/* shows on small screens only (is hidden once screen size is big) */}
-      <select className="block md:hidden">
-        <option value="">Dropdown</option>
-        <option value="/">home</option>
-        <option value="/project-team">project team</option>
-        <option value="/sign-up">Sign Up</option>
-      </select>
+      <div className="block md:hidden">
+        <button onClick={toggleDropdown}>
+          Options
+        </button>
+      </div>
+
+        {isOpen && (
+            <div
+                className="mt-2 w-56 
+                rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5
+                focus:outline-none"
+                role="dropdown"
+            >
+                <div className="py-1" role="none">
+                    <a
+                        href="/project-team"
+                        className="block px-4 py-2 text-sm text-gray-700 
+                        hover:bg-gray-100"
+                        role="dropdownitem"
+                    >
+                        Account settings
+                    </a>
+                    <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700
+                        hover:bg-gray-100"
+                        role="menuitem"
+                    >
+                        Support
+                    </a>
+                    <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700
+                        hover:bg-gray-100"
+                        role="menuitem"
+                    >
+                        License
+                    </a>
+                </div>
+            </div>
+        )}
 
     </div>
   </nav>
