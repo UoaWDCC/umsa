3fcbe46f734b2bae6c8629b1d685030e6ff9a225
Terrence Wu <terrencewu1738@gmail.com>
2026-05-13 17:44:59 +1200

    style: added a hamburger icon

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -35,8 +35,10 @@ export default function Navbar() {
       <div className="relative">
 
       <div className="md:hidden">
-        <button onClick={toggleDropdown}>
-          Dropdown
+        <button onClick={toggleDropdown} className="block lg:hidden p-2 text-white">
+          <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://w3.org">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
         </button>
       </div>
 
