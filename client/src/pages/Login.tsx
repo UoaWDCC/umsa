8a587b503c4f027ac7101565edec35cd674df2dd
jzchenn <jackyc.pata@gmail.com>
2026-05-14 14:13:45 +1200

    feat: tab title change

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Login(){
@@ -9,6 +9,10 @@ export default function Login(){
     console.log("Username:", username);
     console.log("Password:", password);
     }
+    
+    useEffect(() => {
+            document.title = "Login | UMSA";
+            }, []);
 
     return(
         <>
