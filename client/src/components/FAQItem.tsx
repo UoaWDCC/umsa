d28fbb81f5be97bcab9e84932551121b49fd417c
jzchenn <jackyc.pata@gmail.com>
2026-05-15 12:09:26 +1200

    fix: question overflow and arrow alignment

--- client/src/components/FAQItem.tsx
+++ client/src/components/FAQItem.tsx
@@ -13,10 +13,10 @@ export default function FAQItem({ stats }: Props) {
     const [isHidden, setIsHidden] = useState(true);
 
     return(
-        <div className="mb-8 border-b border-white pb-4 w-[90%] mx-auto text-left">
+        <div className="border-b border-white py-6 w-[90%] mx-auto text-left">
             <div className="cursor-pointer flex gap-2" onClick={() => setIsHidden(!isHidden)}>
-                <h2 className="text-xl font-bold mb-2">{stats.question}</h2>
-                <span className="inline-block w-3.5 h-3.5 border-r-2 border-b-2 rotate-45 "></span>
+                <h2 className="text-xl font-bold mb-0 whitespace-normal wrap-break-word flex-1 min-w-0">{stats.question}</h2>
+                <span className="shrink-0 self-center inline-block w-3.5 h-3.5 border-r-2 border-b-2 rotate-45 "></span>
             </div>
             <p className={`text-lg mb-4 ${isHidden ? "hidden" : ""} `}>{stats.answer}</p>
         </div>
