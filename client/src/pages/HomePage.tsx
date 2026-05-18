9b0cd396bad1c8f66680be6ed1736bd296842a53
nryuzl <prunanero@gmail.com>
2026-05-19 11:20:22 +1200

    feat: home page

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export default function HomePage() {
+  useEffect(() => {
+    document.title = "Home | UMSA";
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center text-white px-6 py-12">
+      <h1 className="text-5xl font-bold mb-3">UMSA</h1>
+      <p className="text-white text-lg">
+        University of Auckland Malaysian Students Association
+      </p>
+    </div>
+  );
+}
