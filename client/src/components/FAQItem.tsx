efdde6f6192f3d68548d00ef748230c1bae9c93d
jzchenn <jackyc.pata@gmail.com>
2026-05-21 15:30:37 +1200

    feat: add animated expand/collpase with fade effect.

--- client/src/components/FAQItem.tsx
+++ client/src/components/FAQItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 type FAQItem = {
     question: string;
@@ -11,14 +11,28 @@ type Props = {
 
 export default function FAQItem({ stats }: Props) { 
     const [isHidden, setIsHidden] = useState(true);
+    const answerRef = useRef<HTMLDivElement>(null);
+    const [height, setHeight] = useState("0px")
 
+    useEffect(() => {
+        if (!isHidden && answerRef.current) {
+            setHeight(`${answerRef.current.scrollHeight}px`);
+        } else {
+            setHeight("0px");
+        }
+    
+    }, [!isHidden]);
     return(
-        <div className="border-b border-white py-6 w-[90%] mx-auto text-left">
+        <div className={`border-b border-white py-6 w-[90%] mx-auto text-left faq-container ${isHidden ? "closed" : "open"}`}>
             <div className="cursor-pointer flex gap-2" onClick={() => setIsHidden(!isHidden)}>
                 <h2 className="text-xl font-bold mb-0 whitespace-normal wrap-break-word flex-1 min-w-0">{stats.question}</h2>
-                <span className="shrink-0 self-center inline-block w-3.5 h-3.5 border-r-2 border-b-2 rotate-45 "></span>
+                <span className="shrink-0 self-center inline-block w-3.5 h-3.5 border-r-2 border-b-2 rotate-45 m-0"></span>
             </div>
-            <p className={`text-lg mb-4 ${isHidden ? "hidden" : ""} `}>{stats.answer}</p>
+            <p 
+            ref={answerRef}
+            style={{ height }}
+            className={`text-lg ${isHidden ? "opacity-0" : "opacity-100"} overflow-hidden transition-[height,opacity] duration-300 ease-in-out`}
+            >{stats.answer}</p>
         </div>
     );
 }
