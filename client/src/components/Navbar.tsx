0ef8d86f66c0cc5a6c79a119722fa6bacaff3889
Terrence Wu <terrencewu1738@gmail.com>
2026-05-21 14:06:08 +1200

    fix: nav button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
       </Link>
 
       {/* shows on desktop screens (hidden at first) */}
-      <div className="hidden md:flex gap-6 text-sm">
+      <div className="hidden md:flex items-center gap-6 text-sm">
         <NavLink to="/" end className={linkClass}>Home</NavLink>
         <NavLink to="/About" className={linkClass}> About </NavLink>
         <NavLink to="/Events" className={linkClass}> Events</NavLink>
@@ -37,6 +37,12 @@ export default function Navbar() {
         <NavLink to="/Gallery" className={linkClass}>Gallery</NavLink>
         <NavLink to= "/faq" className={linkClass}> FAQ </NavLink> 
         <NavLink to="/project-team" className={linkClass}>Project Team</NavLink>
+        {/* button to send user to sign up form via response link */}
+        <a href="https://forms.gle/CQoE2nsZ9sGCWZqW8" target="_blank" rel="noopener noreferrer"
+        className="px-6 py-1 rounded-full bg-blue-400 hover:bg-blue-700 hover:scale-110 
+        transisition-transform duration-100">
+          <button type="button" className="text-white font-bold cursor-pointer">Sign Up</button>
+        </a>
       </div>
 
       {/* shows on small screens only (is hidden once screen size is big) */}
@@ -73,7 +79,7 @@ export default function Navbar() {
         <div className="md:hidden absolute mt-2 left-0 w-full justify-items-start rounded-md bg-gray-900
           ring-black ring-opacity-5 focus:outline-none">
           <div>
-            <div className="flex flex-col px-8 py-10 gap-2 text-left whitespace-nowrap">
+            <div className="flex flex-col justify-items-start px-8 py-10 gap-2 text-left whitespace-nowrap">
               <NavLink to="/" end className={linkClass} onClick={() => setIsOpen(false)}>
                 Home
               </NavLink>
@@ -99,7 +105,11 @@ export default function Navbar() {
               <NavLink to="/project-team" className={linkClass} onClick={() => setIsOpen(false)}>
                 Project Team
               </NavLink>
-              
+              <a href="https://forms.gle/CQoE2nsZ9sGCWZqW8" target="_blank" rel="noopener noreferrer" 
+              className="px-4 py-1 mr-1 rounded-full bg-blue-400 hover:bg-blue-700 hover:scale-110 
+              transisition-transform duration-100">
+                <button type="button" className="text-white font-bold cursor-pointer">Sign Up</button>
+              </a>
             </div>
           </div>
           
