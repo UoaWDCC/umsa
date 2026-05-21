42d5c848e17a71a4b6b90b98424ded39ade88b36
Alex <alexl210107@gmail.com>
2026-05-21 14:26:37 +1200

    feat: pagination
    * feat: add Footer + footer function in rootLayout
    
    * Feat: added react-router-hash-link package
    
    * Feat: HashLink used to navigate + page scroll reset to top in Rootlayout, Fix:reinstal package
    
    * Feat: added link to WDCC
    
    * feat: made footer text smaller
    
    * fix: minor design changes
    
    * fix: minor margin fix
    
    * fix: missing ; in events,tsx
    
    * fix: removed package-lock.json and reinstalled react hashlink package
    
    * feat: added pagination + pageNumber component
    
    * feat: changed the name of the EventPageNumber Comp
    
    * feat: grouped pages + arrows to see adjacent groups
    
    * fix: removed test cases
    
    * fix: comments + indentation fixes
    
    ---------
    
    Co-authored-by: alanna <139197719+alannasantoso@users.noreply.github.com>

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -1,59 +1,172 @@
 import { useEffect, useState } from "react";
-import EventsInfo from "../components/EventsElement";
+import EventsInfo from "../components/EventsElement"; 
 import EventImage from "../assets/about-us/Copy of UMSA_Guitar.png";
+import Page from "../components/EventsPageButton";
 
 {
   /* constant events array */
 }
 const events = [
-  {
-    eventName: "Jalinan Raya",
-    eventImage: EventImage,
-    eventLink: "https://www.instagram.com/p/DWPgAxmky6q/?img_index=1",
-    eventDescription: "very cool and very descriptive description",
-    eventIsDone: false,
-    eventDate: new Date("2026-05-20"),
-    eventTag: "Social",
-  },
-  {
-    eventName: "Bersatu Trials",
-    eventImage: EventImage,
-    eventLink: "https://www.instagram.com/p/DV7o4eZmPHH/?img_index=1",
-    eventDescription: "even cooler and even more descriptive description",
-    eventIsDone: false,
-    eventDate: new Date("2026-05-28"),
-    eventTag: "Social",
-  },
-  {
-    eventName: "Clash of UMSA",
-    eventImage: EventImage,
-    eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
-    eventDescription:
-      "even even cooler and way wayyyyy more descriptive description",
-    eventIsDone: false,
-    eventDate: new Date("2026-06-21"),
-    eventTag: "Competition",
-  },
-  {
-    eventName: "Clash of UMSA",
-    eventImage: EventImage,
-    eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
-    eventDescription:
-      "even even cooler and way wayyyyy more descriptive description",
-    eventIsDone: false,
-    eventDate: new Date("2026-03-21"),
-    eventTag: "Social",
-  },
-  {
-    eventName: "Clash of UMSA",
-    eventImage: EventImage,
-    eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
-    eventDescription:
-      "even even cooler and way wayyyyy more descriptive description",
-    eventIsDone: true,
-    eventDate: new Date("2026-04-20"),
-    eventTag: "Competition",
-  },
+    {
+        eventName: "Jalinan Raya",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DWPgAxmky6q/?img_index=1",
+        eventDescription: "very cool and very descriptive description",
+        eventIsDone: false,
+        eventDate: new Date("2026-05-20"),
+        eventTag: "Social",
+        page: 1,
+    },
+    {
+        eventName: "Bersatu Trials",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DV7o4eZmPHH/?img_index=1",
+        eventDescription: "even cooler and even more descriptive description",
+        eventIsDone: false,
+        eventDate: new Date("2026-05-28"),
+        eventTag: "Social",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: false,
+        eventDate: new Date("2026-06-21"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: false,
+        eventDate: new Date("2026-03-21"),
+        eventTag: "Social",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 2026",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2026-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 2025",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2025-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 2024",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2024-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 2023",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2023-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 2022",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2022-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 21",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2021-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 20",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2020-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 19",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2019-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 18",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2018-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 17",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2017-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 16",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2016-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
+    {
+        eventName: "Clash of UMSA 15",
+        eventImage: EventImage,
+        eventLink: "https://www.instagram.com/p/DVrOmhNk_kI/",
+        eventDescription: "even even cooler and way wayyyyy more descriptive description",
+        eventIsDone: true,
+        eventDate: new Date("2015-04-20"),
+        eventTag: "Competition",
+        page: 1,
+    },
 ];
 
 {
@@ -82,6 +195,12 @@ const upcomingEvents = processedEvents.filter(
 );
 const pastEvents = processedEvents.filter((event) => event.eventIsDone == true);
 
+{/* Check how many pages needed to display the past events */}
+const maxItemPage = 6;
+const pastEventsGrouped = pastEvents.map((event, index) => ({ 
+      ...event, page: Math.floor(index / maxItemPage) + 1
+}));
+
 export default function Events() {
   useEffect(() => {
     document.title = "Events | UMSA";
@@ -99,57 +218,107 @@ export default function Events() {
       ? upcomingEvents
       : upcomingEvents.filter((event) => event.eventTag == activeTag);
 
-  const filteredPastEvents =
-    activeTag == "All"
-      ? pastEvents
-      : pastEvents.filter((event) => event.eventTag == activeTag);
+    {/* state variable and setter function using useState with default value All, to rerender the activeTag based on onClick event from button */}
+    const [activeTag, setActiveTag] = useState("All");
+    {/* when initial or new activeTag, EventsArrays are filtered for activeTag */}
+    const filteredUpcomingEvents = 
+        activeTag == "All" ? upcomingEvents : upcomingEvents.filter((event) => event.eventTag == activeTag);
+    const filteredPastEvents = 
+        activeTag == "All" ? pastEventsGrouped : pastEventsGrouped.filter((event) => event.eventTag == activeTag);
 
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col w-4/5" id="upcoming-events-section">
-          <h1 className="text-3xl font-bold my-6">Upcoming Events</h1>
-        </div>
-        <div className="flex flex-wrap-reverse justify-end w-4/5 mb-3 gap-4">
-          <button
-            onClick={() => setActiveTag("All")} //onClick calls setActiveTag function, useState updates - react rerenders - activeTag returns new value
-            className={`w-fit text-sm text-gray-400 hover:text-white cursor-pointer 
-                        ${activeTag == "All" ? "text-white font-bold" : ""}`}
-          >
-            all
-          </button>
-          <button
-            onClick={() => setActiveTag("Social")}
-            className={`w-fit text-sm text-gray-400 hover:text-white cursor-pointer 
-                        ${activeTag == "Social" ? "text-white font-bold" : ""}`}
-          >
-            social
-          </button>
-          <button
-            onClick={() => setActiveTag("Competition")}
-            className={`w-fit text-sm text-gray-400 hover:text-white cursor-pointer 
-                        ${activeTag == "Competition" ? "text-white font-bold" : ""}`}
-          >
-            competition
-          </button>
-        </div>
-        {/* grid only containing upcoming events, using the upcomingEvents array */}
-        <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {filteredUpcomingEvents.map((upcomingEvent) => (
-            <EventsInfo key={upcomingEvent.eventName} element={upcomingEvent} />
-          ))}
-        </div>
+    {/* Filter pastEvent for active page */}
+    const [activePage, setActivePage] = useState(1);
+    const filteredPastEventsPage = filteredPastEvents.filter((event) => event.page == activePage);
 
-        <div className="my-8"></div>
+    {/* make a list with just page numbers for all the page needed */}
+    const filteredPageNum = Math.floor(filteredPastEvents.length / maxItemPage) + 1;
+    const pageButtons = Array.from({ length: filteredPageNum }, (_, index) => ({
+        page: index + 1,
+        setterFunction: (page: number) => setActivePage(page),
+    }));
 
-        <div className="flex flex-col w-4/5" id="past-events-section">
-          <h1 className="text-3xl font-bold my-6">Past Events</h1>
-        </div>
-        {/* grid only containing past events, using the pastEvents array */}
-        <div className="w-4/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {filteredPastEvents.map((pastEvent) => (
-            <EventsInfo key={pastEvent.eventName} element={pastEvent} />
-          ))}
+    {/* useState for current pageGroup */}
+    const [pageGroupOffset, setPageGroupOffset] = useState(0);
+
+    const pagesPerGroup = 5;
+    const startIndex = pageGroupOffset * pagesPerGroup;
+    const visiblePages = pageButtons.slice(startIndex, startIndex + pagesPerGroup);
+    const hasNextGroup = startIndex + pagesPerGroup < pageButtons.length;
+    const hasPrevGroup = pageGroupOffset > 0;
+
+    return(
+        <>
+        <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col w-4/5" id="upcoming-events-section">
+                <h1 className="text-3xl font-bold my-6">
+                    Upcoming Events
+                </h1>
+            </div>
+            <div className="flex flex-wrap-reverse justify-end w-4/5 mb-3 gap-4">
+                <button onClick={() => setActiveTag("All")} //onClick calls setActiveTag function, useState updates - react rerenders - activeTag returns new value
+                    className={
+                        `w-fit text-sm text-gray-400 hover:text-white cursor-pointer 
+                        ${activeTag == "All" ? "text-white font-bold" : ""}`
+                    }>
+                    all
+                </button>
+                <button onClick={() => setActiveTag("Social")} 
+                    className={
+                        `w-fit text-sm text-gray-400 hover:text-white cursor-pointer 
+                        ${activeTag == "Social" ? "text-white font-bold" : ""}`
+                    }>
+                    social
+                </button>
+                <button onClick={() => setActiveTag("Competition")} 
+                    className={
+                        `w-fit text-sm text-gray-400 hover:text-white cursor-pointer 
+                        ${activeTag == "Competition" ? "text-white font-bold" : ""}`
+                    }>
+                    competition
+                </button>
+
+            </div>
+            {/* grid only containing upcoming events, using the upcomingEvents array */}
+            <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {filteredUpcomingEvents.map((upcomingEvent) => (
+                    <EventsInfo key={upcomingEvent.eventName} element={upcomingEvent}/>
+                ))}
+            </div>
+
+            <div className="my-8"></div>
+
+            <div className="flex flex-col w-4/5"  id="past-events-section">
+                <h1 className="text-3xl font-bold my-6">
+                    Past Events
+                </h1>
+            </div>
+            {/* grid only containing past events, using the pastEvents array */}
+            <div className="w-4/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {filteredPastEventsPage.map((pastEvent) => (
+                    <EventsInfo key={pastEvent.eventName} element={pastEvent}/>
+                ))}
+            </div>
+            <div className="flex flex-wrap justify-center w-4/5 mt-4 mb-4 gap-4">
+                <div className="flex flex-row gap-4">
+                    {hasPrevGroup && (
+                        <button onClick={() => setPageGroupOffset(pageGroupOffset - 1)}
+                            className="text-gray-400 hover:text-white"
+                        >
+                            &lt;
+                        </button>
+                    )}
+                    {visiblePages.map((button) => (
+                        <Page key={button.page} element={button} currentPage={activePage} />
+                    ))}
+                    {hasNextGroup && (
+                        <button onClick={() => setPageGroupOffset(pageGroupOffset + 1)}
+                            className="text-gray-400 hover:text-white"
+                        >
+                            &gt;
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
       </div>
     </>
