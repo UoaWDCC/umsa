78a60b110adb1fc9345d02714609e5024ccc30cc
nryuzl <prunanero@gmail.com>
2026-05-26 23:21:13 +1200

    feat: add button to link to execs page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   useEffect(() => {
@@ -11,9 +12,9 @@ export default function HomePage() {
       <p className="text-white text-lg">
         University of Auckland Malaysian Students Association
       </p>
-      <div className="w-full aspect-video">
+      <div className="w-full aspect-video mt-10 mb-10">
         <iframe 
-        className="w-full h-full rounded-lg mt-10"
+        className="w-full h-full rounded-lg"
         src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=uBbsDjvI95vfXOQA"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -21,7 +22,19 @@ export default function HomePage() {
         allowFullScreen>
         </iframe>
       </div>
-      <div className="max-w-xl text-center mb-5"></div>
+
+      <div className="max-w-xl text-center mb-5">
+        <h2 className="text-2xl font-semibold mb-3">Who are we?</h2>
+        <p className="text-white">
+          umsa goated umsa goated umsa goated umsa goated umsa goated umsa goated umsa goated
+        </p>
+        <Link
+          to="/team"
+          className="mt-5 bg-white text-black font-semibold rounded-lg"
+        >
+          Meet the Execs
+        </Link>
+      </div>
     </div>
   );
 }
