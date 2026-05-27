564f9bfbdf83d8c912c691de6e42eb5c5149d041
nryuzl <prunanero@gmail.com>
2026-05-27 14:58:04 +1200

    refactor: style execs link

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -9,30 +9,36 @@ export default function HomePage() {
   return (
     <div className="flex flex-col items-center text-white px-6 py-12">
       <h1 className="text-5xl font-bold mb-3">UMSA</h1>
-      <p className="text-white text-lg">
+      <p className="text-white text-lg mb-3">
         University of Auckland Malaysian Students Association
       </p>
       <div className="w-full aspect-video mt-10 mb-10">
-        <iframe 
-        className="w-full h-full rounded-lg"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=uBbsDjvI95vfXOQA"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen>
-        </iframe>
+        <iframe
+          className="w-full h-full rounded-lg"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=uBbsDjvI95vfXOQA"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
       </div>
 
       <div className="max-w-xl text-center mb-5">
         <h2 className="text-2xl font-semibold mb-3">Who are we?</h2>
-        <p className="text-white">
-          umsa goated umsa goated umsa goated umsa goated umsa goated umsa goated umsa goated
+        <p className="text-white mb-10">
+          umsa goated umsa goated umsa goated umsa goated umsa goated umsa
+          goated umsa goated
         </p>
+      </div>
+
+      <div className="max-w-xl text-center border border-white/50 rounded-lg py-10 px-15">
+        <h2 className="text-2xl font-semibold mb-3">Meet the Exec Team</h2>
+        <p className="text-white mb-10">para abt umsa execs etc</p>
         <Link
           to="/team"
-          className="mt-5 bg-white text-black font-semibold rounded-lg"
+          className="mt-5 bg-white text-black font-semibold rounded-xl p-4"
         >
-          Meet the Execs
+          click here
         </Link>
       </div>
     </div>
