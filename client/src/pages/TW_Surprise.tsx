96986967b688b16a910083e30f2243a0f96af797
Terrence Wu <terrencewu1738@gmail.com>
2026-05-28 14:25:51 +1200

    umsa is so goated

--- client/src/pages/TW_Surprise.tsx
+++ client/src/pages/TW_Surprise.tsx
@@ -1,4 +1,4 @@
-import { easeInOut, motion } from "motion/react";
+import { motion } from "motion/react";
 import tung from "../assets/surprise1.jpeg";
 import umsa from "../assets/UMSA.png";
 
@@ -17,9 +17,8 @@ export default function Surprise() {
         <div className="flex px-5 gap-10 flex-wrap justify-center">
         {images.map((image, index) => (
         <motion.div initial={{opacity:0, scale: 0, y:0}} animate={{opacity:1, scale:1, y:215}} 
-        transition={{duration:3}} exit={{scale:2, y:15}}>
+        transition={{duration:3}} key={index}>
           <img
-            key={index}
             src={image.src}
             alt={image.alt}
             className="w-48 h-48 object-fit animate-bounce"
