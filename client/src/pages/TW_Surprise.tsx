cbefe8e9ed2acd8a4bbeecd4a217d71a371a7b58
Terrence Wu <terrencewu1738@gmail.com>
2026-05-28 14:23:46 +1200

    tung and umsa bonding

--- client/src/pages/TW_Surprise.tsx
+++ client/src/pages/TW_Surprise.tsx
@@ -1,26 +1,30 @@
+import { easeInOut, motion } from "motion/react";
 import tung from "../assets/surprise1.jpeg";
+import umsa from "../assets/UMSA.png";
 
 export default function Surprise() {
 
     const images = [
     { src: tung, alt: "tung" },
-    { src: tung, alt: "tung" },
+    { src: umsa, alt: "tung" },
     { src: tung, alt: "tung" },
     ]
 
     return (
         <>
         <p>SURPRISE</p>
-        <img src="tung" alt="" />
 
         <div className="flex px-5 gap-10 flex-wrap justify-center">
         {images.map((image, index) => (
+        <motion.div initial={{opacity:0, scale: 0, y:0}} animate={{opacity:1, scale:1, y:215}} 
+        transition={{duration:3}} exit={{scale:2, y:15}}>
           <img
             key={index}
             src={image.src}
             alt={image.alt}
-            className="w-48 h-48 object-cover"
+            className="w-48 h-48 object-fit animate-bounce"
           />
+        </motion.div>
         ))}
         </div>
         </>
