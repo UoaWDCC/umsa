c8d687c80d30751076367d7e77c5b9d2d8a13650
Terrence Wu <terrencewu1738@gmail.com>
2026-05-28 14:43:22 +1200

    tung tung saqure in progress

--- client/src/pages/Wordle.tsx
+++ client/src/pages/Wordle.tsx
@@ -1,9 +1,17 @@
+import { motion } from "motion/react";
+
 export default function Wordle() {
+
     return (
     <>
+    {/* lowkye bmight be broken, fix later note to terrence */}
+    <div className="justify-evenly. items-center w-auto h-100">
     <h1>WORDLE!!!!1!!</h1>
     <input className="border border-gray-300 rounded py-2 px-4" />
-    
+    <motion.div initial={{scale:0}} animate={{scale:1}}>
+    <div className="w-20 h-20 bg-gray-250 rounded-2xl"><p>square</p></div>
+    </motion.div>
+    </div>
     {/* for the displaying, as words r of different length, we can just give ech leeter irs own dic
     and then yeah */}
     
