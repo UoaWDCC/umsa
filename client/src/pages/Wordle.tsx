b3486882d990608520717423b65dc885fb63e6d4
Terrence Wu <terrencewu1738@gmail.com>
2026-05-28 14:35:29 +1200

    added note to future terrence

--- client/src/pages/Wordle.tsx
+++ client/src/pages/Wordle.tsx
@@ -3,6 +3,10 @@ export default function Wordle() {
     <>
     <h1>WORDLE!!!!1!!</h1>
     <input className="border border-gray-300 rounded py-2 px-4" />
+    
+    {/* for the displaying, as words r of different length, we can just give ech leeter irs own dic
+    and then yeah */}
+    
     </>
     )
 }
