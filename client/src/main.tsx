af1e75df98641ff1656e44bc3e4953f62a4561ab
jzchenn <jackyc.pata@gmail.com>
2026-06-03 22:16:56 +1200

    feat: routed WoD

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -17,6 +17,7 @@ import Contact from "./pages/Contact";
 import Sponsors from "./pages/Sponsors";
 import FAQ from "./pages/Frequent-Asked-Question";
 import About from "./pages/About";
+import WoD from "./pages/WoD"
 
 {
   /* project team imports */
@@ -50,6 +51,7 @@ const router = createBrowserRouter([
       { path: "contact", element: <Contact /> },
       { path: "sponsors", element: <Sponsors /> },
       { path: "about", element: <About /> },
+      { path: "word-of-the-day", element: <WoD/>}
     ],
   },
 ]);
---
+++ client/src/pages/WoD.tsx
@@ -0,0 +1,7 @@
+export default function WoD() {
+  return (
+    <>
+      <h1>Word of the Day</h1>
+    </>
+  );
+}
