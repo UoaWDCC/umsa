aaf90d4efca453388b6f810502e64a85baf63b15
jzchenn <jackyc.pata@gmail.com>
2026-08-06 14:55:43 +1200

    feat(backend): create event schema

---
+++ server/src/models/Events.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IEventDocument extends Document {
+  name: string;
+  startsAt: Date;
+  description: string;
+  tag: string;
+  link: string;
+  imageUrl: string;
+  imagePublicId: string;
+}
+
+const EventSchema = new Schema<IEventDocument>(
+  {
+    name:          { type: String, required: true },
+    startsAt:      { type: Date,   required: true },
+    description:   { type: String, required: true },
+    tag:           { type: String, required: true },
+    link:          { type: String },
+    imageUrl:      { type: String },
+    imagePublicId: { type: String },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<IEventDocument>("HomeContent", EventSchema);
