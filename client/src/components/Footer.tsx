0aec237c2753ef8c5beebcec0dd3cc1d3005abf8
Terrence Wu <terrencewu1738@gmail.com>
2026-08-15 10:08:06 +1200

    style: reorganized footer to look like design

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,51 +1,49 @@
 import { Link } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link'
-import { FaInstagram, FaFacebook } from "react-icons/fa";
+import { FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
+import umsa2  from "../assets/icons/umsa2.png";
 
 export default function Footer() {
-    const footerLinkClass = "text-left text-gray-400 hover:text-white cursor-pointer"
+    const footerLinkClass = "text-gray-400 hover:text-white cursor-pointer"
 
     return (
         <>
         {/* Footer elements */}
-        <nav className='mx-6 py-4 gap-4 flex flex-col'>
+        <nav className='mx-6 py-4 gap-4 flex flex-row justify-around'>
             {/* Main footer content */}
-            <div className='flex flex-row gap-20 mx-6 text-sm'>
+            <div className='flex flex-row gap-18 mx-6 text-sm'>
                 {/* Page Navigation */}
-                <div className='flex flex-col'>
-                    <p className='text-left font-bold'>Pages</p>
-                    <Link to="/" className={footerLinkClass}>Home</Link>
-                    <Link to="/project-team" className={footerLinkClass}>project team</Link>
-                    <Link to="/sign-up" className={footerLinkClass}>Sign Up</Link>
-                    <Link to="/gallery" className={footerLinkClass}>Gallery</Link>
+                <div className='flex flex-col text-right'>
+                    <Link to="/" className={footerLinkClass}>About Us</Link>
+                    <Link to="/Sponsors" className={footerLinkClass}>Sponsors</Link>
+                    <Link to="/Events" className={footerLinkClass}>Our Events</Link>
+                    <Link to="/Team" className={footerLinkClass}>Meet the Team</Link>
                 </div>
+
+                <img src={umsa2} className="size-30 relative -top-8"/>
 
                 {/* Events Navigation */}
-                <div className='flex flex-col'>
-                    <p className='text-left font-bold'>Events</p>
-                    <HashLink to="/events#upcoming-events-section" className={footerLinkClass}>Upcoming Events</HashLink>
-                    <HashLink to="/events#past-events-section" className={footerLinkClass}>Past Events</HashLink>
-                </div>
+                <div className='flex flex-col text-left'>
+                    <Link to="/Gallery" className={footerLinkClass}>Photos</Link>
+                    <Link to="/Contact" className={footerLinkClass}>Contact Us</Link>
+                
 
                 {/* External Links */}
-                <div className='flex flex-row gap-4 items-center ml-auto'>
+                <div className='flex flex-row gap-4 items-center ml-auto mt-5 mr-13'>
                     <a href="https://www.instagram.com/umsanz/" target="_blank" rel="noopener noreferrer">
-                            <FaInstagram className="text-3xl text-grey-300 hover:text-white" />
+                        <FaInstagram className="text-3xl text-grey-300 hover:text-white" />
                     </a>
                     <a href="https://www.facebook.com/umsanz/" target="_blank" rel="noopener noreferrer">
-                            <FaFacebook className="text-3xl text-grey-300 hover:text-white" />
+                        <FaFacebook className="text-3xl text-grey-300 hover:text-white" />
                     </a>
-                </div>
-            </div>
 
-            {/* Copyright bottom footer */}
-            <div className='flex flex-row justify-between text-sm text-gray-600 mx-6'>
-                <p>
-                    All rights reserved UMSA 2026.
-                </p>
-                <p>
-                    Design and Developed by <a href="https://wdcc.co.nz/" target="_blank" rel="noreferrer" className='underline'>WDCC</a>
-                </p>
+                    {/* THIS ISJ UST A PLACEHOLDER CUZ THEY DONT HAVE A TWITTER AND IT LOOKS BAD WITHOUT
+                    A THIRD ICON */}
+                    <a href="https://www.facebook.com/umsanz/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="text-3xl text-grey-300 hover:text-white" />
+                    </a>
+
+                </div>
+                </div>
             </div>
         </nav>
         </>
