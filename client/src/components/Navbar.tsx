42401add9d005061ef9ceedc0ae75230ebc56fba
minahil saqib <mao@Mac.fritz.box>
2026-08-20 00:16:26 +1200

    Have started on about us page rough draft with headers and mascot

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -47,80 +47,28 @@ export default function Navbar() {
       {/* shows on small screens only (is hidden once screen size is big) */}
       <div className="md:hidden">
         <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
-        <button onClick={toggleDropdown} className="flex flex-col gap-1.5 lg:hidden p-2 text-white">
+          <button onClick={toggleDropdown} className="flex flex-col gap-1.5 lg:hidden p-2 text-white">
+            {/* top line  */}
+            <motion.span
+              animate={isOpen ? { rotate: 45, y: 8 } : { rotate: 0, y: 0 }}
+              className="w-8 h-1 bg-white block transition-colors"
+            />
 
-        {/* shows on desktop screens (hidden at first) */}
-        <div className="hidden md:flex gap-6 text-sm">
-          <NavLink to="/" end className={linkClass}>
-            Home
-          </NavLink>
-          <NavLink to="/About" className={linkClass}>
-            {" "}
-            About{" "}
-          </NavLink>
-          <NavLink to="/Events" className={linkClass}>
-            {" "}
-            Events
-          </NavLink>
-          <NavLink to="/sign-up" className={linkClass}>
-            Sign Up
-          </NavLink>
-          <NavLink to="/Contact" className={linkClass}>
-            Contact
-          </NavLink>
-          <NavLink to="/Team" className={linkClass}>
-            {" "}
-            Team{" "}
-          </NavLink>
-          <NavLink to="/Sponsors" className={linkClass}>
-            {" "}
-            Sponsors{" "}
-          </NavLink>
-          <NavLink to="/Gallery" className={linkClass}>
-            Gallery
-          </NavLink>
-          <NavLink to="/faq" className={linkClass}>
-            {" "}
-            FAQ{" "}
-          </NavLink>
-          <NavLink to="/project-team" className={linkClass}>
-            Project Team
-          </NavLink>
-        </div>
+            {/* middle Line */}
+            <motion.span
+              animate={isOpen ? { opacity: 0 } : { opacity: 1 }}
+              className="w-8 h-1 bg-white block transition-colors"
+            />
 
-        {/* shows on small screens only (is hidden once screen size is big) */}
-        <div className="md:hidden">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-          >
-            <button
-              onClick={toggleDropdown}
-              className="flex flex-col gap-1.5 lg:hidden p-2 text-white"
-            >
-              {/* top line  */}
-              <motion.span
-                animate={isOpen ? { rotate: 45, y: 8 } : { rotate: 0, y: 0 }}
-                className="w-8 h-1 bg-white block transition-colors"
-              />
-
-              {/* middle Line */}
-              <motion.span
-                animate={isOpen ? { opacity: 0 } : { opacity: 1 }}
-                className="w-8 h-1 bg-white block transition-colors"
-              />
-
-              {/* bottom Line */}
-              <motion.span
-                animate={isOpen ? { rotate: -45, y: -12 } : { rotate: 0, y: 0 }}
-                className="w-8 h-1 bg-white block transition-colors"
-              />
-            </button>
-          </motion.div>
-        </div>
+            {/* bottom Line */}
+            <motion.span
+              animate={isOpen ? { rotate: -45, y: -12 } : { rotate: 0, y: 0 }}
+              className="w-8 h-1 bg-white block transition-colors"
+            />
+          </button>
+        </motion.div>
       </div>
-
+    </div>
       {/* if isOpen is true it runs the code in the block */}
       {isOpen && (
         // drop down options
