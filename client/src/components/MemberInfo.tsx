e098c4899bc0d4b143e2a6f6d5e3434572039d92
jzchenn <jackyc.pata@gmail.com>
2026-09-03 17:08:34 +1200

    feat: card responsive

--- client/src/components/MemberInfo.tsx
+++ client/src/components/MemberInfo.tsx
@@ -14,17 +14,17 @@ type Props = {
 
 export default function MemberInfo({ stats }: Props) {
   return (
-    <div className="flex flex-col items-center rounded-[10px] bg-[#7B7BEA] p-3 shadow-md w-[254px] h-[317px]">
+    <div className="@container flex flex-col items-center rounded-[10px] bg-[#7B7BEA] p-3 shadow-md w-52 aspect-3/4">
       <div>
-        <img className="w-[200px] h-[181px] rounded-[10px] object-cover" src={stats.img} />
+        <img className="w-45 h-40 rounded-[10px] object-cover" src={stats.img} />
         <div className="text-left w-full">
-          <div className="mb-2 mt-3 flex items-center space-x-1 text-white text-xl">
+          <div className="mb-2 mt-3 flex items-center space-x-1 text-white text-base">
             <FiHeart></FiHeart>
             <FiMessageCircle></FiMessageCircle>
             <FiSend></FiSend>
           </div>
-          <p className="mb-0 text-lg font-bold leading-none font-display text-[18px]">{stats.role}</p>
-          <p className="mt-1 leading-none font-display text-[14px]">{stats.fullName}</p>
+          <p className="mb-0 leading-none font-display text-sm font-bold">{stats.role}</p>
+          <p className="mt-1 leading-none font-display text-xs text-white/80">{stats.fullName}</p>
         </div>
       </div>
     </div>
