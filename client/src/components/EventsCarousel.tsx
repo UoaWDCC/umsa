be49aac8a204b5b2e8c608eb276a8e93a3fbf239
Twu1738 <terrencewu1738@gmail.com>
2026-09-04 17:28:22 +1200

    style: made disabled buttons look more obvious

--- client/src/components/EventsCarousel.tsx
+++ client/src/components/EventsCarousel.tsx
@@ -16,12 +16,13 @@ export function EventsCarousel() {
     const shiftRight = () => setIndex((i) => Math.min(i + 1, maxIndex));
     const shiftLeft = () => setIndex((i) => Math.max(i - 1, 0)); 
 
+    // takes 3 events from the list of events
     const visibleEvents = events.slice(index, index + VISIBLE)
 
     return(
       <>
         <div className="flex flex-row justify-center items-center gap-5 px-6">
-          <button onClick={shiftLeft} disabled={index === 0}>
+          <button onClick={shiftLeft} disabled={index === 0} className="disabled:opacity-50">
             <img src={left_arrow} className="h-20 w-auto cursor-pointer"/></button>
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 max-w-324">
           {visibleEvents.map((event, counter) =>(
@@ -29,7 +30,7 @@ export function EventsCarousel() {
               aspect-[3/4] object-contain rounded-md ${opacityLevel[counter]}`}/>{event.eventName}</p>
               ))}
           </div>
-          <button onClick={shiftRight} disabled={index === maxIndex}>
+          <button onClick={shiftRight} disabled={index === maxIndex} className="disabled:opacity-50">
             <img src={right_arrow} className="h-20 w-auto cursor-pointer"/></button>
         </div>
       </>
